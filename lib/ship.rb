8b6232eb31f4f58ba8cbc5ff54ecbc6ac0440d4f
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-10-19 21:41:52 -0400

    Feat: add initialize, health, and sunk method to Ship class

---
+++ lib/ship.rb
@@ -0,0 +1,15 @@
+class Ship
+  attr_reader :name, :length
+  def initialize(name, length)
+    @name = name
+    @length = length
+  end
+
+  def health
+    @length
+  end
+
+  def sunk?
+    health == 0
+  end
+end
