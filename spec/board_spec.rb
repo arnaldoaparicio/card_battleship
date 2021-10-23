059fdbc578a9ec2c6aa0613dded0a421041ffd41
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-10-23 19:37:09 -0400

    test: add placing ships test for place method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -50,7 +50,42 @@ RSpec.describe Board do
     expect(board.cells.values.first).to be_an_instance_of(Cell)
 
     expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
-    expect(board.valid_placement?(cruiser, ["A1", "B1", "C1"])).to eq(true)
     expect(board.valid_placement?(submarine, ["A1", "B1", "C1"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+  end
+
+  it 'cannot be diagonal' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    expect(board.cells).to be_a Hash
+    expect(board.cells.keys.length).to eq(16)
+    expect(board.cells.values.first).to be_an_instance_of(Cell)
+
+    expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+
+    expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
+    expect(board.valid_placement?(cruiser, ["A1", "B1", "C1"])).to eq(true)
+    expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
+
+  end
+
+  it 'has placing ships' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
+    expect(cell_1.ship).to eq(cruiser)
+    # cell_2.ship
+    # cell_3.ship
+    #
+    #
+    # cell_3.ship == cell_2.ship
+
   end
 end
