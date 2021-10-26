169a76f38487c2665e7fd05d3a90ba594c9db99f
craig cardwell <wizeduvcc@gmail.com>
2021-10-26 14:35:58 -0600

    feat: add sub_placement, cruiser_placement, and render methods

--- lib/setup.rb
+++ lib/setup.rb
@@ -1,7 +1,3 @@
-require './lib/ship'
-require './lib/cell'
-require './lib/board'
-require './lib/game'
 
 class Setup
   attr_reader :board
@@ -15,9 +11,43 @@ class Setup
   # and how to place ships
   # show new board to player
   # respond to invalid placements
-  def computer_setup
+  def sub_placement(ship, placement=nil)
 
+    x = [["A1", "A2"],["A2", "A3"],["A3", "A4"],["B1", "B2"],
+    ["B2", "B3"],["B3", "B4"],["C1", "C2"],["C2", "C3"],
+    ["C3", "C4"],["D1", "D2"],["D2", "D3"],["D3", "D4"],
+    ["A1", "B1"],["B1", "C1"],["C1", "D1"],["A2", "B2"],
+    ["B2", "C2"],["C2", "D2"],["A3", "B3"],["B3", "C3"],
+    ["C3", "D3"],["A4", "B4"],["B4", "C4"],["C4", "D4"]]
+
+
+    placement = x.sample
+  board.place(ship, placement)
   end
 
+  def cruiser_placement(ship, placement=nil)
+    x = [["A1", "A2", "A3"],["A2", "A3", "A4"],["B1", "B2", "B3"],
+    ["B2", "B3", "B4"],["C1", "C2", "C3"],["C2", "C3", "C4"],
+    ["D1", "D2", "D3"],["D2", "D3", "D4"],["A1", "B1", "C1"],
+    ["B1", "C1", "D1"],["A2", "B2", "C2"],["B2", "C2", "D2"],
+    ["A3", "B3", "C3"],["B3", "C3", "D3"],["A4", "B4", "C4"],
+    ["B4", "C4", "D4"]]
 
+    placement = x.sample do |place|
+    until board.valid_placement?(ship, placement) == true
+    end
+    board.place(ship, placement)
+    end
+  end
+
+  def computer_render
+    p "I have laid out my ships on the grid.\n
+          You now need to lay out your two ships.\n
+          The Cruiser is three units long and the Submarine is two units long.
+
+    #{board.render(ship_present=true)}
+
+     Enter the squares for the Cruiser (3 spaces):\n>"
+    # response = gets.chomp
+  end
 end
