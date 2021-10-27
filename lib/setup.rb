4063dae72c24a2df85c16abe9880a799a91d3297
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-10-26 21:25:31 -0400

    refactor: add player_render and player_placement methods

--- lib/setup.rb
+++ lib/setup.rb
@@ -11,7 +11,7 @@ class Setup
   # and how to place ships
   # show new board to player
   # respond to invalid placements
-  def sub_placement(ship, placement=nil)
+  def sub_placement(ship)
 
     x = [["A1", "A2"],["A2", "A3"],["A3", "A4"],["B1", "B2"],
     ["B2", "B3"],["B3", "B4"],["C1", "C2"],["C2", "C3"],
@@ -22,10 +22,10 @@ class Setup
 
 
     placement = x.sample
-  board.place(ship, placement)
+    board.place(ship, placement)
   end
 
-  def cruiser_placement(ship, placement=nil)
+  def cruiser_placement(ship)
     x = [["A1", "A2", "A3"],["A2", "A3", "A4"],["B1", "B2", "B3"],
     ["B2", "B3", "B4"],["C1", "C2", "C3"],["C2", "C3", "C4"],
     ["D1", "D2", "D3"],["D2", "D3", "D4"],["A1", "B1", "C1"],
@@ -33,21 +33,36 @@ class Setup
     ["A3", "B3", "C3"],["B3", "C3", "D3"],["A4", "B4", "C4"],
     ["B4", "C4", "D4"]]
 
-    placement = x.sample do |place|
+      placement = x.sample do |place|
     until board.valid_placement?(ship, placement) == true
-    end
-    board.place(ship, placement)
+
+      end
+      board.place(ship, placement)
     end
   end
 
   def computer_render
-    p "I have laid out my ships on the grid.\n
-          You now need to lay out your two ships.\n
-          The Cruiser is three units long and the Submarine is two units long.
+    puts board.render(ship_present=false)
+  end
 
-    #{board.render(ship_present=true)}
+  def player_render
+    puts board.render(ship_present=true)
+  end
 
-     Enter the squares for the Cruiser (3 spaces):\n>"
-    # response = gets.chomp
+  def player_placement(ship)
+    x = [["A1", "A2", "A3"],["A2", "A3", "A4"],["B1", "B2", "B3"],
+    ["B2", "B3", "B4"],["C1", "C2", "C3"],["C2", "C3", "C4"],
+    ["D1", "D2", "D3"],["D2", "D3", "D4"],["A1", "B1", "C1"],
+    ["B1", "C1", "D1"],["A2", "B2", "C2"],["B2", "C2", "D2"],
+    ["A3", "B3", "C3"],["B3", "C3", "D3"],["A4", "B4", "C4"],
+    ["B4", "C4", "D4"]]
+    # ship = Ship.new("Cruiser", 3)
+    placement = gets.chomp
+    if x.include?(placement)
+      board.place(ship, placement)
+    else
+      p "Those are invalid coordinates. Please try again:"
+      placement = gets.chomp
+    end
   end
 end
